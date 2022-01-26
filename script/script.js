8418a2a4db9a36d1e7dd710cc14d8c3e11c453f0
Daniel Oliveira <danielcoliv98@gmail.com>
2022-01-26 16:39:31 -0300

    Botao agora ativa quando seleciona os itens

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+document.getElementById("botao1").disabled = true;
+
 var food1 = document.getElementById("food1");
 var food2 = document.getElementById("food2");
 var food3 = document.getElementById("food3");
@@ -8,40 +10,46 @@ var drink2 = document.getElementById("drink2");
 var drink3 = document.getElementById("drink3");
 var drink4 = document.getElementById("drink4");
 
-var food = document.getElementsByClassName("food");
-
-var foodOption = false;
-var drinkOption = false;
-var dessertOption = false;
+var confirm = [];
 
 function selectFood(opcao) {
-    food1.className = "option";
-    food2.className = "option";
-    food3.className = "option";
-    food4.className = "option";
+  food1.className = "option";
+  food2.className = "option";
+  food3.className = "option";
+  food4.className = "option";
 
   opcao.className = "option-selected";
-  foodOption = true;
+  confirm[0] = true;
 }
 
-function selectDrink (opcao) {
+function selectDrink(opcao) {
   drink1.className = "option";
   drink2.className = "option";
   drink3.className = "option";
   drink4.className = "option";
 
-opcao.className = "option-selected";
-drinkOption = true;
+  opcao.className = "option-selected";
+  confirm[1] = true;
 }
 
-function selectDessert (opcao) {
+function selectDessert(opcao) {
   dessert1.className = "option";
   dessert2.className = "option";
   dessert3.className = "option";
   dessert4.className = "option";
 
-opcao.className = "option-selected";
-dessertOption = true;
+  opcao.className = "option-selected";
+  confirm[2] = true;
 }
 
+var clique = document.getElementsByClassName("option");
 
+function verificar(){
+  if(confirm[0] && confirm[1] && confirm[2]){
+    document.getElementById("botao1").disabled = false;
+}
+}
+
+function closeOrder() {
+   console.log("ola");
+}
